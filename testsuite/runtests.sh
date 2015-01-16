e12d7f17f52277211f93992e3c2b13f5085cbe56
Severen Redwood <severen.redwood@gmail.com>
2015-01-16 23:13:24 +1300

    Rewrite dependency checking code to run `dependency-check.rb`

--- testsuite/runtests.sh
+++ testsuite/runtests.sh
@@ -6,5 +6,10 @@
 # or <http://www.gnu.org/licenses/gpl.html>
 #
 
-type ghc > /dev/null 2>&1 || { printf "GHC is not installed!\n"; exit 1; }
-type cabal > /dev/null 2>&1 || { printf "cabal-install is not installed!\n"; exit 1; }
+# TODO: Write tests and benchmarks.
+
+chmod u+x ../util/check-dependencies.rb
+printf "\x1b[33m==>\x1b[0m Checking if Ruby is installed...\n"
+type ruby 2>&1 || { printf "\x1b[31mError:\x1b[0m Ruby is not installed!\n"; exit 1; }
+printf "\x1b[32m==>\x1b[0m Dependency 'ruby' is present.\n\n"
+ruby ../util/check-dependencies.rb
