efa7974c918bad11e0618a68c05cdae40383593c
Severen Redwood <severen.redwood@gmail.com>
2015-01-16 21:59:32 +1300

    Fix indentation in the string containing the usage text

--- src/Pacrat/CLI.hs
+++ src/Pacrat/CLI.hs
@@ -23,18 +23,18 @@ processArgs = putStrLn "Not setup yet"
 usage :: String
 usage = "Name:\n\
      \    pacrat - Making Pacman and the AUR easy to use\n\n\
-	   \Usage:\n\
-	   \    pacrat [global options] command [command options] [arguments...]\n\n\
-	   \Options:\n\
+     \Usage:\n\
+     \    pacrat [global options] command [command options] [arguments...]\n\n\
+     \Options:\n\
      \    -h, --help     :: Display this message\n\
-	   \    -v, --verbose  :: Print extra debug/logging information\n\
-	   \    -V, --version  :: Print version info and exit\n\n\
-	   \Some common Pacrat commands are:\n\
-	   \    install [package(s)]    :: Install one or more packages\n\
-	   \    uninstall [package(s)]  :: Uninstall one or more packages\n\
-	   \    sync                    :: Sync the local Pacman database\n\
-	   \    upgrade                 :: Upgrade packages that have pending updates\n\
-	   \    list                    :: List the currently installed packages\n\
-	   \    gui                     :: Open a package management GUI\n\
-	   \    search [string]         :: Search for a package\n\n\
-	   \See 'pacrat help <command>' for more information on a specific command."
+     \    -v, --verbose  :: Print extra debug/logging information\n\
+     \    -V, --version  :: Print version info and exit\n\n\
+     \Some common Pacrat commands are:\n\
+     \    install [package(s)]    :: Install one or more packages\n\
+     \    uninstall [package(s)]  :: Uninstall one or more packages\n\
+     \    sync                    :: Sync the local Pacman database\n\
+     \    upgrade                 :: Upgrade packages that have pending updates\n\
+     \    list                    :: List the currently installed packages\n\
+     \    gui                     :: Open a package management GUI\n\
+     \    search [string]         :: Search for a package\n\n\
+     \See 'pacrat help <command>' for more information on a specific command."
